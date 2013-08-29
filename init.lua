db8e0a056a828d3696448b1def64b90a445bdd99
Michael 'myrhev' Mathieu <mathieu@cs.nyu.edu>
2013-08-29 13:11:44 -0400

    Fix bug

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ require 'libmongoose'
 
 function mongoose.new(tty)
    local mgdata = torch.Tensor(20):zero()
-   local mg = libmongoose.initMongoose('/dev/ttyUSB0')
+   local mg = libmongoose.initMongoose(tty)
    if mg == nil then
       error("Could not open tty "..tty.." . Please check that the path is right and that you have the correct permission (you might have to join the group dialout).")
    end
@@ -32,7 +32,7 @@ end
 
 function mongoose.getRotmat(mg)
    if mg[2][1] ~= 0 then
-      return mg[2][{{11,19}}]
+      return mg[2][{{11,19}}]:resize(3,3)
    else
       return nil
    end
